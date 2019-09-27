1cc47a012ccc6ad26d35ebf45f4a3cd94cb51e39
Basha <basha16an@gmail.com>
2019-09-27 15:13:15 +0000

    Update MavenBuild.groovy

--- src/com/devlite/gitpipeline/MavenBuild.groovy
+++ src/com/devlite/gitpipeline/MavenBuild.groovy
@@ -8,7 +8,7 @@ class MavenBuild implements Serializable {
   } 
   def buildAppByMaven(mavenBuildEngine){
   
-  workspace=steps.pwd();
+   def workspace=steps.pwd();
   
       if(mavenBuildEngine.buildFile==null || mavenBuildEngine.buildFile==""){
          steps.error "ERROR"
@@ -16,10 +16,10 @@ class MavenBuild implements Serializable {
      steps.sh '''
      set +x 
      cd ''' + workspace + '''
-     export JAVA_HOME='''+mavenBuildEngine.JavaHome+'''
+     export JAVA_HOME='''+ mavenBuildEngine.JavaHome+ '''
      export MAVEN_HOME=/usr/share/maven
      export PATH=$PATH:$MAVEN_HOME/bin:$JAVA_HOM/bin
-     ${MAVEN_HOME}/bin/mvn -f '''+mavenBuildEngine.buildFile+ ''' '''+ mavenBuildEngine.buildTarget
+     ${MAVEN_HOME}/bin/mvn -f '''+ mavenBuildEngine.buildFile + ''' '''+ mavenBuildEngine.buildTarget
      '''
    }
   
