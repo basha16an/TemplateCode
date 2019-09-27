9ed97881198abc36f48a3b233352b29f687aa3aa
Basha <basha16an@gmail.com>
2019-09-27 14:58:25 +0000

    Create MavenBuild.groovy

---
+++ src/com/devlite/gitpipeline/MavenBuild.groovy
@@ -0,0 +1,26 @@
+package com.devlite.gitpipeline;
+import com.devlite.gitpipeline.*;
+class MavenBuild implements Serializable { 
+
+  def steps;
+  MavenBuild(steps) {
+      this.steps = steps
+  } 
+  def buildAppByMaven(mavenBuildEngine){
+  
+  workspace=steps.pwd();
+  
+      if(mavenBuildEngine.buildFile==null || mavenBuildEngine.buildFile==""){
+         steps.error "ERROR"
+     }
+     steps.sh '''
+     set +x 
+     cd ''' + workspace + '''
+     export JAVA_HOME='''+mavenBuildEngine.JavaHome+'''
+     export MAVEN_HOME=/usr/share/maven
+     export PATH=$PATH:$MAVEN_HOME/bin:$JAVA_HOM/bin
+     ${MAVEN_HOME}/bin/mvn -f '''+mavenBuildEngine.buildFile+ ''' '''+ mavenBuildEngine.buildTarget
+     '''
+   }
+  
+}
