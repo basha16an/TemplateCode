6e1479ff8f669926f7af80afc1a2f295317dd72a
Basha <basha16an@gmail.com>
2019-09-27 15:24:00 +0000

    Create SonarScan.groovy

---
+++ src/com/devlite/gitpipeline/SonarScan.groovy
@@ -0,0 +1,26 @@
+package com.devlite.gitpipeline;
+import com.devlite.gitpipeline.*;
+class SonarScan implements Serializable { 
+
+  def steps;
+  SonarScan(steps) {
+      this.steps = steps
+  } 
+  def buildAppByMaven(mavenBuildEngine,URL){
+  
+   def workspace=steps.pwd();
+  
+      if(mavenBuildEngine.buildFile==null || mavenBuildEngine.buildFile==""){
+         steps.error "ERROR"
+     }
+     steps.sh '''
+     set +x 
+     cd ''' + workspace + '''
+     export JAVA_HOME='''+ mavenBuildEngine.JavaHome+ '''
+     export MAVEN_HOME=/usr/share/maven
+     export PATH=$PATH:$MAVEN_HOME/bin:$JAVA_HOM/bin
+     ${MAVEN_HOME}/bin/mvn -f '''+ mavenBuildEngine.buildFile + ''' sonar:sonar -Dsonar.host.url='''+URL+'''
+     '''
+   }
+  
+}
