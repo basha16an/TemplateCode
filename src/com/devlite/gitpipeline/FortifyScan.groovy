12cfbac03a4e0dc1b4fdf3a8f48e7eb78645e57e
Basha <basha16an@gmail.com>
2019-10-09 09:48:42 +0000

    Update FortifyScan.groovy

--- src/com/devlite/gitpipeline/FortifyScan.groovy
+++ src/com/devlite/gitpipeline/FortifyScan.groovy
@@ -34,8 +34,12 @@ class FortifyScan implements Serializable {
      cd ''' + workspace + '''
      export JAVA_HOME='''+ mavenBuildEngine.JavaHome+ '''
      export MAVEN_HOME=/usr/share/maven
-     export PATH=$PATH:$MAVEN_HOME/bin:$JAVA_HOME/bin
-     ${MAVEN_HOME}/bin/mvn -f '''+ mavenBuildEngine.buildFile + ''' '''+ mavenBuildEngine.buildTarget + ''' 
+     export PATH=$PATH:$MAVEN_HOME/bin:$JAVA_HOME/bin:/opt/Fortify/Fortify_SCA_and_Apps_18.20/bin
+     ${MAVEN_HOME}/bin/mvn com.fortify.sca.plugins.maven:sca-maven-plugin:18.20:clean -f '''+mavenBuildEngine.buildFile+'''
+     sourceanalyzer -b '''+mavenBuildEngine.repoName+ ''' mvn package
+     sourceanalyzer -b '''+mavenBuildEngine.repoName+ '''  -scan -f '''+fprScanFilename+ '''
+     BIRTReportGenerator  -template  "DISA STIG" -source '''+fprScanFilename+ ''' -output '''+ pdfScanFilename+ ''' -format PDF -showSuppressed --Version "DISA STIG 3.9" -UseFortifyPriorityOrder
+     
      '''
    }
   
