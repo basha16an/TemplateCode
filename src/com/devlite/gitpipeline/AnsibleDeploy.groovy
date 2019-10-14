ab76eb33769c5e8484be944fde19655928042325
Basha <basha16an@gmail.com>
2019-10-14 05:48:28 +0000

    Update AnsibleDeploy.groovy

--- src/com/devlite/gitpipeline/AnsibleDeploy.groovy
+++ src/com/devlite/gitpipeline/AnsibleDeploy.groovy
@@ -45,7 +45,7 @@ instance_name: testing''', importTowerLogs: true, importWorkflowChildLogs: true,
 	  */
 	  
 	 def ansible_output=steps.ansibleTower async: false, credential: '', extraVars: '''artifact_version: 5.2
-instance_name: testing''', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'AWS_Cloudbees_VM_App_Deploy', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'workflow', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
+instance_name: testing''', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'VMCloudbeesDeploy_HelloWorld', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'workflow', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
 	  println ansible_output.Application_END_URL
 	   // ''' artifact_version: ${build_artifact_version}
 	//  instance_name: ${instance_name} '''
