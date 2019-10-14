ef84145ef5e9720d314e8a4e2bb4a9f67889f363
Basha <basha16an@gmail.com>
2019-10-14 05:26:33 +0000

    Update AnsibleDeploy.groovy

--- src/com/devlite/gitpipeline/AnsibleDeploy.groovy
+++ src/com/devlite/gitpipeline/AnsibleDeploy.groovy
@@ -33,11 +33,16 @@ class AnsibleDeploy implements Serializable {
             artifact_version: 5.2
             instance_name: testing ''', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'VMCloudbeesDeploy_HelloWorld', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'workflow', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
             println ansible_output.Application_END_URL
-	    */
+	    
 	  println workspace
+	  /*
 	  def project = new XmlSlurper().parse(new File("pom.xml"))
        def pomv = project.version.toString()
-	  println pomv
+	  println pomv */
+	  def ansible_output=steps.ansibleTower async: true, credential: '', extraVars: '''artifact_version: 5.2
+host_group: 18.139.219.62
+instance_name: testing''', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'AWS_Cloudbees_VM_App_Deploy', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'job', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
+	  println ansible_output.Application_END_URL
 	   // ''' artifact_version: ${build_artifact_version}
 	//  instance_name: ${instance_name} '''
 	  
