7f86d8e06d253c4ba9baceff69064b22afe757da
Basha <basha16an@gmail.com>
2019-10-27 08:28:43 +0000

    Update AnsibleDeploy.groovy

--- src/com/devlite/gitpipeline/AnsibleDeploy.groovy
+++ src/com/devlite/gitpipeline/AnsibleDeploy.groovy
@@ -51,9 +51,9 @@ def project = new XmlSlurper().parseText(file)
 	  steps.echo "Pom version " +project.version.toString()
 	  steps.echo  "Group IP:"+ project.groupId.toString()
 	  steps.echo "Artifact ID:" +project.artifactId.toString()
-	  def ansible_output=steps.ansibleTower async: false, credential: '', extraVars: '', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'DEVLITE_Workflow_Cloudbees', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'workflow', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
+	//  def ansible_output=steps.ansibleTower async: false, credential: '', extraVars: '', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'DEVLITE_Workflow_Cloudbees', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'workflow', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
 	  
-	   def dev_instance_ENDURL=ansible_output.dev_instance_ENDURL
+	  // def dev_instance_ENDURL=ansible_output.dev_instance_ENDURL
             def test_instance_ENDURL=ansible_output.test_instance_ENDURL
 	  steps.echo dev_instance_ENDURL
 	  
