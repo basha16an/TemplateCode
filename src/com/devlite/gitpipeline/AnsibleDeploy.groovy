6e67f269a3baa8fe72fffe7fae3d9faaf753e47e
Basha <basha16an@gmail.com>
2019-10-27 12:57:01 +0000

    Update AnsibleDeploy.groovy

--- src/com/devlite/gitpipeline/AnsibleDeploy.groovy
+++ src/com/devlite/gitpipeline/AnsibleDeploy.groovy
@@ -58,7 +58,13 @@ return project.version.text()
          //   def test_instance_ENDURL=ansible_output.test_instance_ENDURL
 	  //steps.echo dev_instance_ENDURL
 	  try {
-	  steps.ansibleTower async: false, credential: '', extraVars: '', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'DEVLITE_Application_Deploy', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'job', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
+	 // steps.ansibleTower async: false, credential: '', extraVars: '', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'DEVLITE_Application_Deploy', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'job', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
+		  def ansible_output=steps.ansibleTower async: false, credential: '', extraVars: '''artifact_version: ${build_artifact_version}
+instance_name: ${instance_name}
+dev_instance_count: ${DevInstances}
+test_instance_count: ${TestInstances}
+prod_instance_count: ${ProdIntances}''', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'DEVLITE_Workflow_Cloudbees', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'workflow', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
+		  
 		  } catch (err) {
             echo "Error when test Unit Test"
             throw err
