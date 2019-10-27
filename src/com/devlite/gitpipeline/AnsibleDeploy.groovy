42747e03fbafce34b9e58ec4e123ad4b2ecc348f
Basha <basha16an@gmail.com>
2019-10-27 14:59:18 +0000

    Update AnsibleDeploy.groovy

--- src/com/devlite/gitpipeline/AnsibleDeploy.groovy
+++ src/com/devlite/gitpipeline/AnsibleDeploy.groovy
@@ -26,22 +26,11 @@ class AnsibleDeploy implements Serializable {
      }
     
   }
-  @NonCPS
   def performAnsibleDeployment(mavenBuildEngine){
-  
-   def workspace=steps.pwd();
-   
-   if(mavenBuildEngine.buildFile==null || mavenBuildEngine.buildFile==""){
+     	def workspace=steps.pwd();
+  	 if(mavenBuildEngine.buildFile==null || mavenBuildEngine.buildFile==""){
          steps.error "ERROR"
-     }/*
-	  def ansible_output=steps.ansibleTower credential: '', extraVars: '''
-            artifact_version: 5.2
-            instance_name: testing ''', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'VMCloudbeesDeploy_HelloWorld', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'workflow', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
-            println ansible_output.Application_END_URL
-	    
-	  println workspace
-	  */
-	//  ${build_artifact_version}
+     	}
 	  steps.echo params.instance_name
 	  def file = steps.readFile(workspace +"/pom.xml")
           def project = new XmlSlurper().parseText(file)
@@ -62,48 +51,10 @@ class AnsibleDeploy implements Serializable {
 	  """
 	  steps.echo Parameters
 	  try {
-	//steps.ansibleTower async: false, credential: '', extraVars: '', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'DEVLITE_Application_Deploy', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'job', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true 
-	 // steps.ansibleTower async: false, credential: '', extraVars: '', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'DEVLITE_Application_Deploy', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'job', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
-	//	 def ansible_output=steps.ansibleTower async: false, credential: '', extraVars: '', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'DEVLITE_Workflow_Cloudbees', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'workflow', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
-		  
-		  } catch (err) {
-            steps.echo "Error when test Unit Test"
+	} catch (err) {
+            steps.echo "ERROR: Please Check the Ansible Tower piipeline Coonfiuration and input parameter values"
             throw err
         }
-	  /*
-	  
-	  def ansible_output=steps.ansibleTower async: false, credential: '', extraVars: '''artifact_version: 5.2
-host_group: 18.139.219.62
-instance_name: testing''', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'AWS_Cloudbees_VM_App_Deploy', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'job', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
-	  */
-	  /*
-	 def ansible_output=steps.ansibleTower async: false, credential: '', extraVars: '''artifact_version: 5.2
-instance_name: testing''', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'VMCloudbeesDeploy_HelloWorld', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'workflow', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
-	  println ansible_output.Application_END_URL */
-	  
-	  
-	   // ''' artifact_version: ${build_artifact_version}
-	//  instance_name: ${instance_name} '''
-	  
-      //      Application_deployed_url=ansible_output.Application_END_URL
-      
-       
-       
-	//  def githuburl = "https://github.com/wipropoc/helloworld.git"
-          //def urlFields=githuburl.tokenize('/');
-	  //println (urlFields(countTokens()-1))
-	 // for (String basha: Repo)
-	    //      println (basha)
-		//  def Reponames=Repo.split('.');
-	  //def Reponame=Reponames(0);
-	  //println ("Reponame" +Reponame)
-	  
-	  //step.sh '''echo "https://github.com/wipropoc/helloworld.git" | awk -F "/" '{print $NF}' | awk -F "." '{print $1}'
-    // def projectconfig = new XmlSlurper().parse(new File(workspace+"/"+mavenBuildEngine.buildFile)) ;
-   //def pomversion = projectconfig.version.toString()
-	//
-	//  println("Pom Version:" + pomversion)
-     
-   }
-  
+	//step.sh '''echo "https://github.com/wipropoc/helloworld.git" | awk -F "/" '{print $NF}' | awk -F "." '{print $1}'
+     }
 }
