288889d13bdea678ef390e1f4732f5f318370f7b
Basha <basha16an@gmail.com>
2019-10-28 10:13:34 +0000

    Update NotifyMail.groovy

--- src/com/devlite/gitpipeline/NotifyMail.groovy
+++ src/com/devlite/gitpipeline/NotifyMail.groovy
@@ -23,19 +23,19 @@ class NotifyMail implements Serializable {
 
         Please find the input parameter values:
         --------------
-	      instance_name   = instance_name 
-	      Gitcodeurl      = Gitcodeurl
-	      GitBranch       = GitBranch
-	      Language        = Language  
-	      Languageversion = Languageversion	
-	      ServerType      = ServerType	
-	      FieldType	      = FieldType	
-	      DevInstances    = $DevInstances	
-	      DTshirtsize     = DTshirtsize	
-	      TestInstances   = TestInstances
-	      TTshirtsize     = TTshirtsize	
-	      ProdInstances   = ProdInstances	
-	      MAILIDs		      = MAILIDs	
+	      instance_name   = '''+params.instance_name+'''
+	      Gitcodeurl      = '''+params.Gitcodeurl+'''
+	      GitBranch       = '''+params.GitBranch+'''
+	      Language        = '''+params.Language+'''
+	      Languageversion = '''+params.Languageversion+'''	
+	      ServerType      = '''+params.ServerType+'''
+	      FieldType	      = '''+params.FieldType+'''
+	      DevInstances    = '''+params.DevInstances+'''
+	      DTshirtsize     = '''+params.DTshirtsize+'''
+	      TestInstances   = '''+params.TestInstances+'''
+	      TTshirtsize     = '''+params.TTshirtsize+'''
+	      ProdInstances   = '''+params.ProdInstances+'''	
+	      MAILIDs	      = '''+params.MAILIDs+'''
   Thanks,
   Devops Team ''', replyTo: 'no-reply@wipro-poc.com',
                         from:'no-reply@wipro-poc.com',
