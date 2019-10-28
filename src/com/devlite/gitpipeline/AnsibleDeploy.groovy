a66fe2643fda58f8b7a5a21e38d00fa433119922
Basha <basha16an@gmail.com>
2019-10-28 08:50:25 +0000

    Update AnsibleDeploy.groovy

--- src/com/devlite/gitpipeline/AnsibleDeploy.groovy
+++ src/com/devlite/gitpipeline/AnsibleDeploy.groovy
@@ -58,7 +58,7 @@ class AnsibleDeploy implements Serializable {
 	  """
 	  steps.echo Parameters 
 	  steps.ansibleTower async: false, credential: '', extraVars: '''
-	  artifact_version: '''+ artifact_version + '''
+	  artifact_version: ${params.DevInstances}
 	  ''', importTowerLogs: true, importWorkflowChildLogs: true, inventory: '', jobTags: '', jobTemplate: 'DEVLITE_Workflow_Cloudbees', jobType: 'run', limit: '', removeColor: true, skipJobTags: '', templateType: 'workflow', throwExceptionWhenFail: true, towerServer: 'AnsibleTower', verbose: true
 	//step.sh '''echo "https://github.com/wipropoc/helloworld.git" | awk -F "/" '{print $NF}' | awk -F "." '{print $1}'
      }
