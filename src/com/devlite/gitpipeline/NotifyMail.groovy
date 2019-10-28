b37081ca503f9a4cc3bb2d1e636863b864895cbf
Basha <basha16an@gmail.com>
2019-10-28 09:38:28 +0000

    Update NotifyMail.groovy

--- src/com/devlite/gitpipeline/NotifyMail.groovy
+++ src/com/devlite/gitpipeline/NotifyMail.groovy
@@ -14,16 +14,36 @@ class NotifyMail implements Serializable {
   }
   def sendMail(){
   
-  //def mailSubject="The job $JOB_NAME with $BUILD_DISPLAY_NAME Succeeded"
-  steps.emailext attachLog: true, body: '''Hi Team, 
-
-The Latest Build jo successful.
-
-Thanks,
-Basha''', mimeType: 'test/html', replyTo: 'noreply@wdevlitepoc.com', subject: 'test', to: 'shaik.basha35@wipro.com'
-  }
+   steps.emailext subject: 'The HelloWorld App - Build Status: $BUILD_DISPLAY_NAME has Failed'
+   body: '''
+    Hi All,
   
-  
+        The Current Build BUILD_DISPLAY_NAME is Failed.
+        Please Find  the attached Build Logs: BUILD_URL
+
+        Please find the input parameter values:
+        --------------
+	      instance_name   = instance_name 
+	      Gitcodeurl      = Gitcodeurl
+	      GitBranch       = GitBranch
+	      Language        = Language  
+	      Languageversion = Languageversion	
+	      ServerType      = ServerType	
+	      FieldType	      = FieldType	
+	      DevInstances    = $DevInstances	
+	      DTshirtsize     = DTshirtsize	
+	      TestInstances   = TestInstances
+	      TTshirtsize     = TTshirtsize	
+	      ProdInstances   = ProdInstances	
+	      MAILIDs		      = MAILIDs	
+  Thanks,
+  Devops Team ''' ,
+                        replyTo: 'no-reply@wipro-poc.com',
+                        from:'no-reply@wipro-poc.com',
+                        to: params.MAILIDs,
+                        attachLog:'true'
+                        //attachmentsPattern:'*.pdf'
+               
   
   
  }
