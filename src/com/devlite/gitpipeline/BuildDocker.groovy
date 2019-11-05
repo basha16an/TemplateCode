0411daf185569432460f5f268853b14465f89315
Basha <basha16an@gmail.com>
2019-11-05 09:48:38 +0000

    Update BuildDocker.groovy

--- src/com/devlite/gitpipeline/BuildDocker.groovy
+++ src/com/devlite/gitpipeline/BuildDocker.groovy
@@ -35,7 +35,7 @@ class BuildDocker implements Serializable {
     def pomartifactId=project.artifactId.text().toString()
     def registry = "devlite"
     def registryCredential = 'dockerhub'
-    def repositoryName=registry + "/" + pomartifactId + ":" + artifact_version  
+    def repositoryName=registry + "/" + pomartifactId + ":" + artifact_version
     def dockerApacheImage=docker.build('devlite')
     docker.withRegistry( '', registryCredential ) {
                          dockerApacheImage.push()
