c1503bde4824323105bf906bcdd70f0c49dd104e
Basha <basha16an@gmail.com>
2020-03-03 09:40:27 +0000

    Update BuildDocker.groovy

--- src/com/devlite/gitpipeline/BuildDocker.groovy
+++ src/com/devlite/gitpipeline/BuildDocker.groovy
@@ -6,7 +6,7 @@ class BuildDocker implements Serializable {
   def steps;
 	//def docker;
   BuildDocker(steps) {
-      this.steps = steps-
+      this.steps = steps
 	  //this.docker=docker
 	  
   } 
