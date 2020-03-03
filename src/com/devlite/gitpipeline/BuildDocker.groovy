c45675a00f3cf8d5f3c4dd6510c4a4eda9898a8a
Basha <basha16an@gmail.com>
2020-03-03 11:03:46 +0000

    Update BuildDocker.groovy

--- src/com/devlite/gitpipeline/BuildDocker.groovy
+++ src/com/devlite/gitpipeline/BuildDocker.groovy
@@ -39,7 +39,7 @@ class BuildDocker implements Serializable {
 	 // artifact_version
     def dockerApacheImage=dockerbuild.build(repositoryName)
    // dockerbuild.withDockerRegistry( '', registryCredential ) {
-                         dockerbuild.build(repositoryName).push()
+                    //     dockerbuild.build(repositoryName).push()
                   //       }
    // steps.sh ''' /usr/bin/docker rmi -f '''+repositoryName+ ''' ''' 
     }
