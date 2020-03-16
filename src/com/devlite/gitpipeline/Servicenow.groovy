12881aa8befa17ed8912912a93a8372b30324619
Basha <basha16an@gmail.com>
2020-03-16 05:42:54 +0000

    Update Servicenow.groovy

--- src/com/devlite/gitpipeline/Servicenow.groovy
+++ src/com/devlite/gitpipeline/Servicenow.groovy
@@ -9,10 +9,10 @@ class Servicenow implements Serializable {
   def CreateServiceNowTicket(){
   steps.echo '**********Checkout the code from GIT Started**********'
 
-          SUBJECT="Hellowrold Job with Build has been Initiated"
-          build_description=" Hi Team , \n \n" + SUBJECT + " \n Please  find the BUILD URL:  \n Job Name:  JOB_NAME  \n \n Thanks, \n Wipro DevOps Team"
+        //  SUBJECT="Hellowrold Job with Build has been Initiated"
+        //  build_description=" Hi Team , \n \n" + SUBJECT + " \n Please  find the BUILD URL:  \n Job Name:  JOB_NAME  \n \n Thanks, \n Wipro DevOps Team"
      //    steps.echo ' ' + build_description + '' 
-          def request= steps.ChangeRequest assignedTo:'Ansible Integrations',category:'Other',ci:'AS400',impact:'3 - Low',fullDescription: build_description,Description: build_description,priority:'4 - Low',requestedBy:'Ansible Integrations',risk:'Moderate',shortDescription:SUBJECT,state:'New',type:'Standard'
+          def request= steps.ChangeRequest assignedTo:'Ansible Integrations',category:'Other',ci:'AS400',impact:'3 - Low',fullDescription: build_description,Description: "testing",priority:'4 - Low',requestedBy:'Ansible Integrations',risk:'Moderate',shortDescription:"testing",state:'New',type:'Standard'
          steps.createChangeRequest changeRequest: request
  
     steps.echo '**********Checkout the code from GIT Completed**********'
