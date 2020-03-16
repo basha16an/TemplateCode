77d820b6b3f1c43bc7e58cbf21bf315e49a30529
Basha <basha16an@gmail.com>
2020-03-16 05:44:37 +0000

    Update Servicenow.groovy

--- src/com/devlite/gitpipeline/Servicenow.groovy
+++ src/com/devlite/gitpipeline/Servicenow.groovy
@@ -12,7 +12,7 @@ class Servicenow implements Serializable {
         //  SUBJECT="Hellowrold Job with Build has been Initiated"
         //  build_description=" Hi Team , \n \n" + SUBJECT + " \n Please  find the BUILD URL:  \n Job Name:  JOB_NAME  \n \n Thanks, \n Wipro DevOps Team"
      //    steps.echo ' ' + build_description + '' 
-          def request= steps.ChangeRequest assignedTo:'Ansible Integrations',category:'Other',ci:'AS400',impact:'3 - Low',fullDescription: build_description,Description: "testing",priority:'4 - Low',requestedBy:'Ansible Integrations',risk:'Moderate',shortDescription:"testing",state:'New',type:'Standard'
+          def request= steps.ChangeRequest assignedTo:'Ansible Integrations',category:'Other',ci:'AS400',impact:'3 - Low',fullDescription: "testing",Description: "testing",priority:'4 - Low',requestedBy:'Ansible Integrations',risk:'Moderate',shortDescription:"testing",state:'New',type:'Standard'
          steps.createChangeRequest changeRequest: request
  
     steps.echo '**********Checkout the code from GIT Completed**********'
