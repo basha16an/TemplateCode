867aea69dd38cfaf645e6b0bcdaa4f4554ace884
Basha <basha16an@gmail.com>
2020-03-16 05:54:04 +0000

    Update Servicenow.groovy

--- src/com/devlite/gitpipeline/Servicenow.groovy
+++ src/com/devlite/gitpipeline/Servicenow.groovy
@@ -10,7 +10,7 @@ class Servicenow implements Serializable {
   steps.echo '**********Checkout the code from GIT Started**********'
 
         def SUBJECT="Hellowrold Job with Build has been Initiated"
-         def  build_description=" Hi Team , \n \n" + SUBJECT + " \n Please  find the BUILD URL:  \n Job Name:  JOB_NAME  \n \n Thanks, \n Wipro DevOps Team"
+         def build_description=" Hi Team , \n \n" + SUBJECT + " \n Please  find the BUILD URL:  \n Job Name:  JOB_NAME  \n \n Thanks, \n Wipro DevOps Team"
      //    steps.echo ' ' + build_description + '' 
           def request= steps.ChangeRequest assignedTo:'Ansible Integrations',category:'Other',ci:'AS400',impact:'3 - Low',fullDescription: build_description,Description: build_description,priority:'4 - Low',requestedBy:'Ansible Integrations',risk:'Moderate',shortDescription: SUBJECT,state:'New',type:'Standard'
          steps.createChangeRequest changeRequest: request
